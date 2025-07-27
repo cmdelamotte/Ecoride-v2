77300e654e80de5aadf7e43ffd16f519f39a4205
Charles <charlesmax.delamotte@gmail.com>
2025-07-27 03:28:07 +0200

    feat: implement chart.js in layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -22,6 +22,7 @@
     <?php include __DIR__ . '/partials/footer.php'; ?>
 
     <!-- Scripts JavaScript globaux -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script type="module" src="/js/utils/displayFlashMessage.js"></script>
     <!-- Section pour les scripts spécifiques à la page -->
